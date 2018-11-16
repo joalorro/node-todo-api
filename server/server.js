37e7a91aa26e710369bdefaa1cee24fd74c1a9b7
joalorro <joalorro@gmail.com>
2018-11-16 14:25:25 -0500

    added tests for GET todos

--- server/server.js
+++ server/server.js
@@ -10,14 +10,36 @@ const app = express()
 // Using middleware
 app.use(bodyParser.json())
 
+app.get('/todos', (req,res) => {
+	Todo.find().then( (todos) => {
+		console.log("GET /todos status OK");
+		const formattedData = todos.map( (t) => {
+			return {
+				_id: t._id,
+				text: t.text,
+				completed: t.completed,
+				completedAt: t.completedAt
+			}
+		})
+		res.send({ todos })
+	}, (e) => {
+		console.log("ERROR with GET /todos");
+		res.status(400).send(e)
+	})
+})
+
 app.post('/todos', (req,res) => {
 	const todo = new Todo({
 		text: req.body.text
 	})
 
 	todo.save().then( (todo) => {
+		console.log('POST /todos status CREATED')
+		console.log(todo);
 		res.status(201).send(todo)
 	}, (e) => {
+		console.log('POST /todos status BAD REQUEST')
+		console.log(e);
 		res.status(400).send(e)
 	} )
 })
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,9 +5,30 @@ const { app } = require('../server')
 const { Todo } = require('../models/Todo')
 
 // allows us to run some code before tests run
+
+const todos = [ {
+	"text": 'first test todo'
+}, {
+	"text": "second test todo"
+}]
+
 beforeEach( (done) => {
 	// destroys all todos
-	Todo.remove( {} ).then( () => done())
+	Todo.remove( {} ).then( () => {
+		return Todo.insertMany(todos)
+	}).then( () => done() )
+})
+
+describe('GET /todos', () => {
+	it('it should return all todos', (done) => {
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect( (res) => {
+			expect(res.body.todos.length).toBe(2)
+		})
+		.end(done)
+	})
 })
 
 describe('POST /todos', () => {
@@ -24,7 +45,7 @@ describe('POST /todos', () => {
 			.end( (err, res) => {
 				if (err) return done(err)
 
-				Todo.find().then( (todos) => {
+				Todo.find({text}).then( (todos) => {
 					expect(todos.length).toBe(1)
 					expect(todos[0].text).toBe(text)
 
@@ -38,12 +59,11 @@ describe('POST /todos', () => {
 		request(app)
 		.post('/todos')
 		.send({})
-		.expect(400)
-		.end( (err,res) => {
+	.end( (err,res) => {
 			if (err) return done(err)
 
 			Todo.find().then( (todos) => {
-				expect(todos.length).toBe(0)
+				expect(todos.length).toBe(2)
 
 				done()
 			}).catch( (err) => done(err))
