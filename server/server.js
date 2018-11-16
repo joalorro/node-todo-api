80eb668a9a4164b87edbbd26591e7306fba82219
joalorro <joalorro@gmail.com>
2018-11-16 13:42:33 -0500

    adding tests

--- server/server.js
+++ server/server.js
@@ -1,3 +1,44 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
 const { User } = require('./models/User')
+
+const app = express()
+
+// Using middleware
+app.use(bodyParser.json())
+
+app.post('/todos', (req,res) => {
+	const todo = new Todo({
+		text: req.body.text
+	})
+
+	todo.save().then( (todo) => {
+		res.status(201).send(todo)
+	}, (e) => {
+		res.status(400).send(e)
+	} )
+})
+
+app.post('/users', (req,res) => {
+	const user = new User({
+		email: req.body.email
+	})
+
+	user.save().then( (doc) => {
+		console.log('saving user',doc);
+		res.status(201).send(doc)
+	}, (e) => {
+		console.log('unable to save user',e);
+		res.status(400).send(e)
+	})
+})
+
+// Takes in the port number as its first argument
+
+app.listen(3000, () => {
+	console.log('Starting on port 3000');
+})
+
