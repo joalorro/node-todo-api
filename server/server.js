fc5df62c49315621cb24166a877b0b2f4e2c1176
joalorro <joalorro@gmail.com>
2018-11-18 11:33:43 -0500

    handling 503

--- server/server.js
+++ server/server.js
@@ -2,13 +2,12 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { ObjectID } = require('mongodb')
 
-const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/Todo')
 const { User } = require('./models/User')
 
 const app = express()
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // Using middleware
 app.use(bodyParser.json())
